10f97a0cf58a99a929e9ea32bb6942312daf52c4
4s2r9 <93366838+4s2r9@users.noreply.github.com>
2022-06-28 16:59:07 +0900

    Create DBConnect.class.php

---
+++ src/classes/DBConnect.class.php
@@ -0,0 +1,26 @@
+<?php
+
+class DBConnect {
+  
+  /* Propaties */
+  const DB[NAME] = 'cri_sortable';
+  const DB[HOST] = 'localhost';
+  const DB[CHAR] = 'utf8';
+  const USER     = 'root';
+  const PASS     = 'root';
+
+  /* Methods */
+  public function pdo(){
+    $dsn  = "mysql:dbname=" . self::DB[NAME] . "; host=" . self::DB[HOST] . "; charset=" . self::DB[CHAR];
+    $user = self::USER;
+    $pass = self::PASS;
+    $opt  = [];
+    try{
+      $pdo = new PDO($dsn, $user, $pass, $opt);
+    }catch(Exception $e){
+      echo 'Error occurred: '.$e->getMessage;
+      die();
+    }
+    return $pdo;
+  }
+}
