d1abe9868088d17dbd2924cff4ccd99d8be55161
4s2r9 <93366838+4s2r9@users.noreply.github.com>
2022-06-28 16:37:27 +0900

    Update DB.class.php

--- src/classes/DB.class.php
+++ src/classes/DB.class.php
@@ -1,8 +1,29 @@
 <?php
 
 class DB {
-  private int $id;
+  
+  /* Propaties */
+  private int    $id;
   private String $name;
   private String $email;
   private String $password;
+  private String $theme
+  private int    $genders_id;
   
+  /* functions */
+  
+  // get propaties
+  public int    get__id()         { return $this->id; }
+  public String get__name()       { return $this->name; }
+  public String get__email()      { return $this->email; }
+  public String get__password()   { return $this->password; }
+  public String get__theme()      { return $this->theme; }
+  public int    get__genders_id() { return $this->genders_id; }
+  
+  // set propaties
+  public void set__id($id)                 { $this->id         = $id; }
+  public void set__name($name)             { $this->name       = $name; }
+  public void set__email($email)           { $this->email      = $email; }
+  public void set__password($password)     { $this->password   = $password; }
+  public void set__theme($theme)           { $this->theme      = $theme; }
+  public void set__genders_id($genders_id) { $this->genders_id = $genders_id; }
