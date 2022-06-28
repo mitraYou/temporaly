eef2c9d2071184ab1c87f537f11f38aca946d16d
YusukeTeo <siroha323@gmail.com>
2022-06-28 16:40:25 +0900

    jsの修正とDBフォルダの追加

--- sample/js/test.js
+++ sample/js/test.js
@@ -1,19 +1,19 @@
 
 
 //仮の配列
-var array1 = new Array('リンゴ', 'バナナ', 'イチゴ');
-var random_first = Math.floor( Math.random() * array1.length );
+let array1 = new Array('リンゴ', 'バナナ', 'イチゴ');
+let random_first = Math.floor( Math.random() * array1.length );
 console.log(random_first);
 
 //初期質問
-var Question = document.getElementById("question");
+let Question = document.getElementById("question");
 Question.innerHTML=array1[1];
 
 //nextクリック後の質問
 checkButton.addEventListener("click",function(){
 
   //クリックイベント内でもう一度ランドしないと更新してくれない
-  var random = Math.floor( Math.random() * array1.length );
+  let random = Math.floor( Math.random() * array1.length );
   Question.innerHTML = (array1[random]);
   console.log(value.value);
   console.log(random);
