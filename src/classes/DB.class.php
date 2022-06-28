97d7533011e8d76ee8a42ce97538fe930b9fb2ab
4s2r9 <93366838+4s2r9@users.noreply.github.com>
2022-06-28 16:52:22 +0900

    Update DB.class.php

--- src/classes/DB.class.php
+++ src/classes/DB.class.php
@@ -12,20 +12,22 @@ class DB {
   
   /* functions */
   
+  public function 
+
   // get propaties
-  public int    get__id()         { return $this->id; }
-  public String get__name()       { return $this->name; }
-  public String get__email()      { return $this->email; }
-  public String get__password()   { return $this->password; }
-  public String get__theme()      { return $this->theme; }
-  public int    get__genders_id() { return $this->genders_id; }
+  public function get__id()         : int      { return $this->id; }
+  public function get__name()       : String   { return $this->name; }
+  public function get__email()      : String   { return $this->email; }
+  public function get__password()   : String   { return $this->password; }
+  public function get__theme()      : String   { return $this->theme; }
+  public function get__genders_id() : int      { return $this->genders_id; }
   
   // set propaties
-  public void set__id($id)                 { $this->id         = $id; }
-  public void set__name($name)             { $this->name       = $name; }
-  public void set__email($email)           { $this->email      = $email; }
-  public void set__password($password)     { $this->password   = $password; }
-  public void set__theme($theme)           { $this->theme      = $theme; }
-  public void set__genders_id($genders_id) { $this->genders_id = $genders_id; }
+  public function set__id($id)                 { $this->id         = $id; }
+  public function set__name($name)             { $this->name       = $name; }
+  public function set__email($email)           { $this->email      = $email; }
+  public function set__password($password)     { $this->password   = $password; }
+  public function set__theme($theme)           { $this->theme      = $theme; }
+  public function set__genders_id($genders_id) { $this->genders_id = $genders_id; }
 
 }
