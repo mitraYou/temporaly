2e772c8135c2be8a8f94edb7fa17d629c0295531
YusukeTeo <siroha323@gmail.com>
2022-06-29 22:37:48 +0900

    env setting

--- sample/js/test.js
+++ sample/js/test.js
@@ -1,23 +1,22 @@
+// 仮の配列
+// eslint-disable-next-line no-array-constructor
+const array1 = new Array('リンゴ', 'バナナ', 'イチゴ')
 
+const random_first = Math.floor(Math.random() * array1.length)
+console.log(random_first)
 
-//仮の配列
-let array1 = new Array('リンゴ', 'バナナ', 'イチゴ');
-let random_first = Math.floor( Math.random() * array1.length );
-console.log(random_first);
+// 初期質問
+const Question = document.getElementById('question')
+Question.innerHTML = array1[1]
 
-//初期質問
-let Question = document.getElementById("question");
-Question.innerHTML=array1[1];
+// nextクリック後の質問
 
-//nextクリック後の質問
-checkButton.addEventListener("click",function(){
+checkButton.addEventListener('click', function () {
+  // クリックイベント内でもう一度ランドしないと更新してくれない
+  const random = Math.floor(Math.random() * array1.length)
+  Question.innerHTML = array1[random]
+  console.log(value.value)
+  console.log(random)
+})
 
-  //クリックイベント内でもう一度ランドしないと更新してくれない
-  let random = Math.floor( Math.random() * array1.length );
-  Question.innerHTML = (array1[random]);
-  console.log(value.value);
-  console.log(random);
-
-});
-
-kazu.innerHTML =array1.length;
+kazu.innerHTML = array1.length
