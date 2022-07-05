a4e8c5ea40703863fddea3e8c0e2aa0e224d1bfb
naga <nagarebosi429@gmail.com>
2022-07-05 15:31:34 +0900

    db require追加

--- src/get_questions.php
+++ src/get_questions.php
@@ -1,9 +1,7 @@
 <?php
-$dsn = 'mysql:host=localhost;dbname=u22;charset=utf8mb4';
-$db_user = 'root';
-$db_password = '';
-$pdo = new PDO($dsn, $db_user, $db_password,);
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+require ("db_connect.php");
+
 $results = [];
 for ($i=0; $i < 5; $i++) {
     $sql = 'SELECT * FROM questions WHERE weight = ' . $i . ' ORDER BY id = rand() LIMIT 1';
--- src/set_choice.php
+++ src/set_choice.php
@@ -22,11 +22,8 @@ if($totalling <= 3){
 
 ////totallingに結果を入れてください
 
-$dsn = 'mysql:host=localhost;dbname=u22;charset=utf8mb4';
-$db_user = 'root';
-$db_password = '';
-$pdo = new PDO($dsn, $db_user, $db_password,);
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+require ("db_connect.php");
+
 $sql = 'INSERT INTO users_answers_questions (users_id,questions_id,choice) VALUES (:users_id,:questions_id,:choice)';
 $stmt = $pdo->prepare($sql);
 for ($i=0; $i < 5; $i++) { 
