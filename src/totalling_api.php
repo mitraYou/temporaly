5cedbf48759609f7e4901f0b988b8aa32d2d91be
ikasumi0617@gmail.com <64143893+haminem@users.noreply.github.com>
2022-07-05 15:31:14 +0900

    totallingのapi実装

--- src/totalling_api.php
+++ src/totalling_api.php
@@ -5,14 +5,8 @@ $db_password = '';
 $pdo = new PDO($dsn, $db_user, $db_password,);
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-$sql = 'SELECT * FROM users_answers_questions WHERE weight = ' . $i . ' ORDER BY id = rand() LIMIT 1';
-
-
-$sql = 'INSERT INTO users_answers_questions (users_id,questions_id,choice) VALUES (:users_id,:questions_id,:choice)';
+$sql = 'SELECT totalling FROM statuses WHERE user_id = :user_id';
 $stmt = $pdo->prepare($sql);
-for ($i = 0; $i < 5; $i++) {
-    $stmt->bindValue(':users_id', $data['users_id']);
-    $stmt->bindValue(':questions_id', $data['questions_id'][$i]);
-    $stmt->bindValue(':choice', $data['choice'][$i]);
-    $stmt->execute();
-}
+$stmt->bindValue(':users_id', $data['users_id']);
+$stmt->execute();
+echo $stmt->fetch();
