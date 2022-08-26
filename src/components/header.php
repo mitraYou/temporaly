159648882e077de1e7584821bd60e67588681899
YusukeTeo <siroha323@gmail.com>
2022-08-27 00:52:36 +0900

    setting画像追加、header修正、registrationのCSS変更

--- src/components/header.php
+++ src/components/header.php
@@ -1,13 +1,13 @@
 <nav>
 	<div class='logo'>
-		<img src='images/logo.png' alt='' class="logo_img">
+		<a href="index.php"><img src='images/logo.png' alt='' class="logo_img"></a>
 	</div>
 	<!-- もしセッションがあるなら -->
 	<?php if(isset($_SESSION['user_id'])): ?>
 		<ul>
 			<li><a href="chat.php"><img src="images/hukidasi.png" class="settei_icon"></a></li>
 			<li><a href=""><img src="images/calendar.png" class="settei_icon"></a></li>
-			<li><a href='#'><img src='images/icon.png' class='settei_icon'></a></li>
+			<li><a href='mypage.php'><img src='images/setting.png' class='settei_icon'></a></li>
 		</ul>
 	<?php else: ?>
 		<ul>
